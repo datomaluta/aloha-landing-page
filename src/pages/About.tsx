3e79f7a7fc4b89a4c5ef0fdc88496f862523ad62
datomaluta <datomaluta@gmail.com>
2025-08-03 23:19:40 +0400

    disable team component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,3 @@
-import OurTeam from "../components/about/OurTeam";
 import Statistics from "../components/home/Statistics";
 import WhyChooseUs from "../components/home/WhyChooseUs";
 import DefaultLayout from "../layout/DefaultLayout";
@@ -8,7 +7,7 @@ const About = () => {
     <DefaultLayout>
       <WhyChooseUs about />
       <Statistics />
-      <OurTeam />
+      {/* <OurTeam /> */}
     </DefaultLayout>
   );
 };
